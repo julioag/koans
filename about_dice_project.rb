12e54416218e77c02a724f591f3cc695bfa3cac6
Julio Andrade <jandrade@buk.cl>
2026-07-01 21:02:23 -0400

    class_methods, dice_project, inheritance, modules and scope

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,17 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+
+  def roll(times)
+    arr = []
+    (1..times).each do |_|
+      arr.push(rand(1..6))
+    end
+    @values = arr
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
